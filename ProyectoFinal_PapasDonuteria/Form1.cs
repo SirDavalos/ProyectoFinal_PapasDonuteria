6573fd7af77f4ae29b9ee459353dac66df32cea6
Héctor <162389155+W7Raptor@users.noreply.github.com>
2024-12-16 00:10:07 -0600

    Form1.cs

--- ProyectoFinal_PapasDonuteria/Form1.cs
+++ ProyectoFinal_PapasDonuteria/Form1.cs
@@ -1,18 +1,80 @@
-namespace ProyectoFinal_PapasDonuteria
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PortadaApp
 {
-    public partial class Form1 : Form
+    public class PortadaForm : Form
     {
-        public Form1()
+        public PortadaForm()
         {
-            InitializeComponent();
+
+            this.Text = "Portada";
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.White;
+
+ 
+            Label universidadLabel = new Label();
+            universidadLabel.Text = "Universidad Autónoma de Aguascalientes";
+            universidadLabel.Font = new Font("Arial", 18, FontStyle.Bold);
+            universidadLabel.TextAlign = ContentAlignment.MiddleCenter;
+            universidadLabel.Dock = DockStyle.Top;
+            universidadLabel.Height = 50;
+            this.Controls.Add(universidadLabel);
+
+
+            PictureBox logoPictureBox = new PictureBox();
+            logoPictureBox.Image = Image.FromFile("ruta_del_logo.png"); //debo cambiar la ruta de la imagen, me cago en mi ptm
+            logoPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            logoPictureBox.Size = new Size(200, 200);
+            logoPictureBox.Location = new Point((this.ClientSize.Width - logoPictureBox.Width) / 2, 70);
+            logoPictureBox.Anchor = AnchorStyles.Top;
+            this.Controls.Add(logoPictureBox);
+
+
+            Label nombresLabel = new Label();
+            nombresLabel.Text = "- Héctor Guerrero Villa\n- Oziel\n- Fernando Dávalos Gutiérrez\n- Ely\n- Diego"; //falta añadir bien los nombres
+            nombresLabel.Font = new Font("Arial", 14);
+            nombresLabel.TextAlign = ContentAlignment.TopLeft;
+            nombresLabel.Location = new Point(50, 300);
+            nombresLabel.Size = new Size(this.ClientSize.Width - 100, 150);
+            nombresLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+            this.Controls.Add(nombresLabel);
+
+  
+            Button iniciarButton = new Button();
+            iniciarButton.Text = "Iniciar";
+            iniciarButton.Font = new Font("Arial", 12);
+            iniciarButton.Size = new Size(100, 40);
+            iniciarButton.Location = new Point((this.ClientSize.Width - iniciarButton.Width) / 2, 470);
+            iniciarButton.Anchor = AnchorStyles.Top;
+            iniciarButton.Click += (sender, e) =>
+            {
+                Form siguienteFormulario = new Form2(); 
+                this.Hide();
+                siguienteFormulario.ShowDialog();
+                this.Show();
+            };
+            this.Controls.Add(iniciarButton);
+
+     
+            Button salirButton = new Button();
+            salirButton.Text = "Salir";
+            salirButton.Font = new Font("Arial", 12);
+            salirButton.Size = new Size(100, 40);
+            salirButton.Location = new Point(20, this.ClientSize.Height - 60);
+            salirButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            salirButton.Click += (sender, e) => this.Close();
+            this.Controls.Add(salirButton);
         }
 
-        private void buttonMenu_Click(object sender, EventArgs e)
+        [STAThread]
+        static void Main()
         {
-            Form2 form2 = new Form2();
-            this.Hide();
-            form2.ShowDialog();
-            this.Show();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PortadaForm());
         }
     }
 }
