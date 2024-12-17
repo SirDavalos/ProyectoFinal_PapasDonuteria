62d0964291bc1e2cd6b2156e87b78c13e06e175d
DiegoVenreis <diegovene1603@gmail.com>
2024-12-16 19:44:00 -0600

    metodos

---
+++ ProyectoFinal_PapasDonuteria/Form6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal_PapasDonuteria
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPago_Click(object sender, EventArgs e)
+        {
+            Form8 form8 = new Form8();
+            this.Hide();
+            form8.ShowDialog();
+            this.Show();
+        }
+    }
+}
