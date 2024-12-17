1e8ccaff25c9f793117a5d3d123ec2779fc2e1b8
Héctor <162389155+W7Raptor@users.noreply.github.com>
2024-12-17 08:53:32 -0600

    Update Form1.Designer.cs

--- ProyectoFinal_PapasDonuteria/Form1.Designer.cs
+++ ProyectoFinal_PapasDonuteria/Form1.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace ProyectoFinal_PapasDonuteria
+﻿﻿namespace ProyectoFinal_PapasDonuteria
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,39 +15,36 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            buttonMenu = new Button();
-            SuspendLayout();
+            this.buttonMenu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
             // buttonMenu
             // 
-            buttonMenu.Location = new Point(455, 464);
-            buttonMenu.Name = "buttonMenu";
-            buttonMenu.Size = new Size(133, 32);
-            buttonMenu.TabIndex = 0;
-            buttonMenu.Text = "Ir a Menu";
-            buttonMenu.UseVisualStyleBackColor = true;
-            buttonMenu.Click += buttonMenu_Click;
+            this.buttonMenu.Location = new System.Drawing.Point(455, 464);
+            this.buttonMenu.Name = "buttonMenu";
+            this.buttonMenu.Size = new System.Drawing.Size(133, 32);
+            this.buttonMenu.TabIndex = 0;
+            this.buttonMenu.Text = "Ir a Menu";
+            this.buttonMenu.UseVisualStyleBackColor = true;
+            this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1009, 508);
-            Controls.Add(buttonMenu);
-            Name = "Form1";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Form1";
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 508);
+            this.Controls.Add(this.buttonMenu);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
         }
 
         #endregion
 
-        private Button buttonMenu;
+        private System.Windows.Forms.Button buttonMenu;
     }
 }
+
