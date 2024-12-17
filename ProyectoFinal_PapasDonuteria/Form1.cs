0d68612b5757184f76b56a3ebe19b18f5fe8c9ad
Héctor <162389155+W7Raptor@users.noreply.github.com>
2024-12-17 08:56:16 -0600

    Update Form1.cs

--- ProyectoFinal_PapasDonuteria/Form1.cs
+++ ProyectoFinal_PapasDonuteria/Form1.cs
@@ -8,9 +8,8 @@ namespace ProyectoFinal_PapasDonuteria
     {
         public Form1()
         {
-            InitializeComponent(); 
-            
-          
+            InitializeComponent();
+
             Label universidadLabel = new Label();
             universidadLabel.Text = "Universidad Autónoma de Aguascalientes";
             universidadLabel.Font = new Font("Arial", 18, FontStyle.Bold);
@@ -19,14 +18,12 @@ namespace ProyectoFinal_PapasDonuteria
             universidadLabel.Height = 50;
             this.Controls.Add(universidadLabel);
 
-  
             Label nombresLabel = new Label();
             nombresLabel.Text = "- Héctor Guerrero Villa\n- Angel Oziel Carrillo Díaz de León\n- Fernando Dávalos Gutiérrez\n- A. E. Domínguez Estrada\n- Diego Emilio Venegas Flores";
             nombresLabel.Font = new Font("Arial", 14);
             nombresLabel.Location = new Point(50, 300);
             nombresLabel.Size = new Size(this.ClientSize.Width - 100, 150);
             this.Controls.Add(nombresLabel);
-
 
             Button iniciarButton = new Button();
             iniciarButton.Text = "Iniciar";
@@ -35,12 +32,20 @@ namespace ProyectoFinal_PapasDonuteria
             iniciarButton.Location = new Point((this.ClientSize.Width - iniciarButton.Width) / 2, 470);
             iniciarButton.Click += (sender, e) =>
             {
-                Form form2 = new Form2(); 
+                Form form2 = new Form2();
                 this.Hide();
                 form2.ShowDialog();
                 this.Show();
             };
             this.Controls.Add(iniciarButton);
+
+            Button buttonMenu = new Button();
+            buttonMenu.Text = "Ir a Menú";
+            buttonMenu.Font = new Font("Arial", 12);
+            buttonMenu.Size = new Size(133, 32);
+            buttonMenu.Location = new Point(455, 464);
+            buttonMenu.Click += buttonMenu_Click;
+            this.Controls.Add(buttonMenu);
         }
 
         private void buttonMenu_Click(object sender, EventArgs e)
@@ -49,4 +54,3 @@ namespace ProyectoFinal_PapasDonuteria
         }
     }
 }
-
