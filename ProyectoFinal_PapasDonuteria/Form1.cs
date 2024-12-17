77e90853e135d31213562dee554746b11b44b3e9
Héctor <162389155+W7Raptor@users.noreply.github.com>
2024-12-17 08:32:54 -0600

    Update Form1.cs

--- ProyectoFinal_PapasDonuteria/Form1.cs
+++ ProyectoFinal_PapasDonuteria/Form1.cs
@@ -2,19 +2,15 @@ using System;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace PortadaApp
+namespace ProyectoFinal_PapasDonuteria
 {
-    public class PortadaForm : Form
+    public partial class Form1 : Form
     {
-        public PortadaForm()
+        public Form1()
         {
-
-            this.Text = "Portada";
-            this.Size = new Size(800, 600);
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.BackColor = Color.White;
-
- 
+            InitializeComponent(); 
+            
+          
             Label universidadLabel = new Label();
             universidadLabel.Text = "Universidad Autónoma de Aguascalientes";
             universidadLabel.Font = new Font("Arial", 18, FontStyle.Bold);
@@ -23,58 +19,34 @@ namespace PortadaApp
             universidadLabel.Height = 50;
             this.Controls.Add(universidadLabel);
 
-
-            PictureBox logoPictureBox = new PictureBox();
-            logoPictureBox.Image = Image.FromFile("ruta_del_logo.png"); 
-            logoPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
-            logoPictureBox.Size = new Size(200, 200);
-            logoPictureBox.Location = new Point((this.ClientSize.Width - logoPictureBox.Width) / 2, 70);
-            logoPictureBox.Anchor = AnchorStyles.Top;
-            this.Controls.Add(logoPictureBox);
-
-
+  
             Label nombresLabel = new Label();
-            nombresLabel.Text = "- Héctor Guerrero Villa\n- Angel Oziel Carrillo Díaz de León\n- Fernando Dávalos Gutiérrez\n- A. E. Domínguez Estrada\n- Diego Emilio Venegas Flores"; 
+            nombresLabel.Text = "- Héctor Guerrero Villa\n- Angel Oziel Carrillo Díaz de León\n- Fernando Dávalos Gutiérrez\n- A. E. Domínguez Estrada\n- Diego Emilio Venegas Flores";
             nombresLabel.Font = new Font("Arial", 14);
-            nombresLabel.TextAlign = ContentAlignment.TopLeft;
             nombresLabel.Location = new Point(50, 300);
             nombresLabel.Size = new Size(this.ClientSize.Width - 100, 150);
-            nombresLabel.Anchor = AnchorStyles.Top | AnchorStyles.Left;
             this.Controls.Add(nombresLabel);
 
-  
+
             Button iniciarButton = new Button();
             iniciarButton.Text = "Iniciar";
             iniciarButton.Font = new Font("Arial", 12);
             iniciarButton.Size = new Size(100, 40);
             iniciarButton.Location = new Point((this.ClientSize.Width - iniciarButton.Width) / 2, 470);
-            iniciarButton.Anchor = AnchorStyles.Top;
             iniciarButton.Click += (sender, e) =>
             {
-                Form siguienteFormulario = new Form2(); 
+                Form form2 = new Form2(); 
                 this.Hide();
-                siguienteFormulario.ShowDialog();
+                form2.ShowDialog();
                 this.Show();
             };
             this.Controls.Add(iniciarButton);
-
-     
-            Button salirButton = new Button();
-            salirButton.Text = "Salir";
-            salirButton.Font = new Font("Arial", 12);
-            salirButton.Size = new Size(100, 40);
-            salirButton.Location = new Point(20, this.ClientSize.Height - 60);
-            salirButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            salirButton.Click += (sender, e) => this.Close();
-            this.Controls.Add(salirButton);
         }
 
-        [STAThread]
-        static void Main()
+        private void buttonMenu_Click(object sender, EventArgs e)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PortadaForm());
+            MessageBox.Show("Este botón lleva al menú.");
         }
     }
 }
+
