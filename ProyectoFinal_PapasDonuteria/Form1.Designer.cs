831685d1616df7fe9d21934fd4103c8a16737a5c
Héctor <162389155+W7Raptor@users.noreply.github.com>
2024-12-17 08:56:43 -0600

    Update Form1.Designer.cs

--- ProyectoFinal_PapasDonuteria/Form1.Designer.cs
+++ ProyectoFinal_PapasDonuteria/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿﻿namespace ProyectoFinal_PapasDonuteria
+﻿namespace ProyectoFinal_PapasDonuteria
 {
     partial class Form1
     {
@@ -17,34 +17,16 @@
 
         private void InitializeComponent()
         {
-            this.buttonMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // buttonMenu
-            // 
-            this.buttonMenu.Location = new System.Drawing.Point(455, 464);
-            this.buttonMenu.Name = "buttonMenu";
-            this.buttonMenu.Size = new System.Drawing.Size(133, 32);
-            this.buttonMenu.TabIndex = 0;
-            this.buttonMenu.Text = "Ir a Menu";
-            this.buttonMenu.UseVisualStyleBackColor = true;
-            this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
-            // 
-            // Form1
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1009, 508);
-            this.Controls.Add(this.buttonMenu);
-            this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Name = "Form1";
             this.ResumeLayout(false);
         }
 
         #endregion
-
-        private System.Windows.Forms.Button buttonMenu;
     }
 }
-
